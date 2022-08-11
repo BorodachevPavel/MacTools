dc34637e8fbe0319719756a84eef9a9aec79d0ae
pavel <p.borodachev@gmail.com>
2022-08-11 21:41:48 +0500

    firstcommit

---
+++ installMacTools.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Install useful tools
+# brew should be installed first
+
+brew install mc
+brew install iterm2
+brew install tree
+brew install --cask sublime-text
+brew install obsidian
+brew install dbeaver
+brew install subethaedit
+brew install --cask firefox
+brew install --cask mos
+brew install --cask karabiner-elements
